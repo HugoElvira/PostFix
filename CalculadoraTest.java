b5389638a1b5eff2992b5e0915f59d7eeb52bbe5
GerardoCardoza <car15410@uvg.edu.gt>
2016-07-25 11:03:26 -0600

    Add files via upload

---
+++ CalculadoraTest.java
@@ -0,0 +1,77 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author GerardoEnrique
+ */
+public class CalculadoraTest {
+    
+    public CalculadoraTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of Calcular method, of class Calculadora.
+     */
+    @Test
+    public void testCalcular() {
+        System.out.println("Calcular");
+        String vector = "";
+        Calculadora instance = new Calculadora();
+        int expResult = 0;
+        int result = instance.Calcular(vector);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of ToString method, of class Calculadora.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("ToString");
+        Calculadora instance = new Calculadora();
+        String expResult = "";
+        String result = instance.ToString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of LeerArchivo method, of class Calculadora.
+     */
+    @Test
+    public void testLeerArchivo() {
+        System.out.println("LeerArchivo");
+        String direccion = "";
+        Calculadora instance = new Calculadora();
+        String expResult = "";
+        String result = instance.LeerArchivo(direccion);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
